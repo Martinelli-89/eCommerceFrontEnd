2a4a6b029ed90dad2adc8a9be5baedd65d5ddb1e
Alex Martinelli <alexmartinelli1102@gmail.com>
2024-09-26 16:42:24 +0100

    Organized basic layout of landing page

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="nav">
+        Nav goes here, logo on the left, basket on the right
+      </div>
+      <div className="main">
+        Main content here
+      </div>
+      <div className="footer">
+        Footer with company info
+      </div>
     </div>
   );
 }
