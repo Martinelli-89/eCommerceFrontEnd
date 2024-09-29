0e9ee17aa9acbb822449f9a9d95c6e1c3d4ec985
Alex Martinelli <alexmartinelli1102@gmail.com>
2024-09-29 15:52:27 +0100

    added more layuot for landing page

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -3,6 +3,7 @@ import menuIcon from "../src/Assets/Images/menuIcon.svg";
 import searchIcon from "../src/Assets/Images/searchIcon.svg";
 import basketIcon from "../src/Assets/Images/bagIcon.svg";
 import userIcon from "../src/Assets/Images/userIcon.svg";
+import background from "../src/Assets/Images/mainPagePhoto.jpg";
 
 const App = () => {
   return (
@@ -10,15 +11,17 @@ const App = () => {
       <section className='header'>
         <img className='header__menu' src={menuIcon} alt="menu icon" />
         <img className='header__search' src={searchIcon} alt="search icon" />
-        <h2 className='header__logo'>AM</h2>
+        <h2 className='header__logo'>EW</h2>
         <img className='header__user' src={userIcon} alt="user icon" />
         <img className='header__basket' src={basketIcon} alt="basket icon" />
       </section>
       <section className='main'>
-        Main here
+        <p>EVERYWARE</p>
+        <img className='main__background' src={background} alt="background photo" />
+        <p>SINCE 2024</p>
       </section>
       <section className='footer'>
-        Basket here
+        Footer here
       </section>
     </div>
   );
