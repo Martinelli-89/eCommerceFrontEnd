3a384bdff11e950d40b978c8b93c3b15ef6ad16e
Alex Martinelli <alexmartinelli1102@gmail.com>
2024-10-11 11:49:57 +0100

    Added link to main page

--- front_end/src/Components/Nav/Nav.js
+++ front_end/src/Components/Nav/Nav.js
@@ -1,4 +1,5 @@
 import './Nav.scss';
+import { Link } from "react-router-dom";
 import menuIcon from "../../../src/Assets/Images/menuIcon.svg";
 import searchIcon from "../../../src/Assets/Images/searchIcon.svg";
 import basketIcon from "../../../src/Assets/Images/bagIcon.svg";
@@ -10,7 +11,9 @@ const Nav = () => {
         <section className='header'>
             <img className='header__menu' src={menuIcon} alt="menu icon" />
             <img className='header__search' src={searchIcon} alt="search icon" />
-            <h2 className='header__logo brand'>EW</h2>
+            <Link to="*" style={{ textDecoration: 'none',  color:'inherit' }}>
+                <h2 className='header__logo brand'>EW</h2>
+            </Link>
             <img className='header__user' src={userIcon} alt="user icon" />
             <img className='header__basket' src={basketIcon} alt="basket icon" />
         </section>
